0271417677ea7733647a54b4d40224ebf1bd0432
Timothy Beck <johndaveleet@gmail.com>
2019-09-11 13:58:58 -0600

    added missing semi-colons

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ function initColorPicker() {
 function setColorPickerEventListeners(colorBox, rgb) {
 	rgb.red.addEventListener('change', () => {
 		console.log("Red Value: ", rgb.red.value);
-	})
+	});
 	rgb.green.addEventListener('change', () => {
 		console.log("Green Value: ", rgb.green.value);
-	})
+	});
 	rgb.blue.addEventListener('change', () => {
 		console.log("Blue Value: ", rgb.blue.value);
-	})
+	});
 }
