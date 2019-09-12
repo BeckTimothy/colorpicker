026d683c0a912bd31aabc871e195ccf9432b017e
Timothy Beck <johndaveleet@gmail.com>
2019-09-12 12:34:19 -0600

    added function that modifies the value of the box color when changes are made to event listeners

--- script.js
+++ script.js
@@ -15,11 +15,19 @@ function initColorPicker() {
 function setColorPickerEventListeners(colorBox, rgb) {
 	rgb.red.addEventListener('change', () => {
 		console.log("Red Value: ", rgb.red.value);
+		setBoxColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 	rgb.green.addEventListener('change', () => {
 		console.log("Green Value: ", rgb.green.value);
+		setBoxColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 	rgb.blue.addEventListener('change', () => {
 		console.log("Blue Value: ", rgb.blue.value);
+		setBoxColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
+}
+
+function setBoxColor(colorBox, red, green, blue) {
+	rgbVal = [red, green, blue].join(',');
+	colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
 }
